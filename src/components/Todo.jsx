78dd2fb99dff16684c8436f88b003449f718ddbf
Robert Katz <robertk@fslt231.cdiconsulting.com>
2023-09-15 15:08:15 -0400

    task #1

---
+++ src/components/Todo.jsx
@@ -0,0 +1,10 @@
+const Todo = () => {
+    return (
+        <div>
+            <h1>Walk the dog</h1>
+            <input type="checkbox" value="false">Completed</input>
+        </div>
+    );
+}
+
+export default Todo;
